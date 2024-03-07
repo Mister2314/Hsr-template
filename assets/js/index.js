df5d6a0959c1a06e072e908ba987a28aa877d71e
Mister2314 <xeyalhuseynli06@gmail.com>
2024-03-07 17:18:07 +0400

    All bugs fixed

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,10 +4,19 @@ document.addEventListener("DOMContentLoaded", function () {
     var volumeBtn = document.querySelector(".volume-share .left-pg");
     var music = document.getElementById("backgroundMusic");
 
-    // Müziği otomatik olarak başlat
-    music.play();
+    // Müziği başlangıçta durdur
+    music.pause();
 
-    // Ses açma/kapatma düğmesine basıldığında müziği kontrol et
+    // Video durdurma/başlatma düğmesi
+    playPauseVideoBtn.addEventListener("change", function () {
+        if (playPauseVideoBtn.checked) {
+            video.play();
+        } else {
+            video.pause();
+        }
+    });
+
+    // Ses açma/kapama düğmesi
     volumeBtn.addEventListener("click", function () {
         var onImg = volumeBtn.querySelector(".on");
         var offImg = volumeBtn.querySelector(".off");
@@ -15,16 +24,15 @@ document.addEventListener("DOMContentLoaded", function () {
         if (onImg.style.display === "none") {
             onImg.style.display = "block";
             offImg.style.display = "none";
-            music.muted = false; // Müziği aç
+            music.muted = false;
+            localStorage.setItem("muteStatus", "unmuted");
+            music.play(); // Müziği başlat
         } else {
             onImg.style.display = "none";
             offImg.style.display = "block";
-            music.muted = true; // Müziği kapat
+            music.muted = true;
+            localStorage.setItem("muteStatus", "muted");
+            music.pause(); // Müziği durdur
         }
-    });
-
-    // Sayfa yenilendiğinde müziği tekrar başlat
-    window.addEventListener("load", function() {
-        music.play();
     });
 });
