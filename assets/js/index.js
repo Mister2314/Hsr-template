b52a4deee2f398164a89ebafdecd9678117e7f88
Mister2314 <xeyalhuseynli06@gmail.com>
2024-07-18 17:43:16 +0400

    Autoplay errors fixed

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,7 +4,29 @@ document.addEventListener("DOMContentLoaded", function () {
     var volumeBtn = document.querySelector(".volume-share .left-pg");
     var music = document.getElementById("backgroundMusic");
 
-    music.pause();
+    // Sayfa yüklendiğinde yerel depolamadan kontrol et ve başlangıçta sesi açık yap
+    var muteStatus = localStorage.getItem("muteStatus");
+    if (muteStatus === "muted") {
+        var onImg = volumeBtn.querySelector(".on");
+        var offImg = volumeBtn.querySelector(".off");
+        onImg.style.display = "none";
+        offImg.style.display = "block";
+        music.muted = true;
+    } else {
+        var onImg = volumeBtn.querySelector(".on");
+        var offImg = volumeBtn.querySelector(".off");
+        onImg.style.display = "block";
+        offImg.style.display = "none";
+        music.muted = false;
+        music.play();
+    }
+
+    // Video ve müzik durumunu kontrol et
+    if (playPauseVideoBtn.checked) {
+        video.play();
+    } else {
+        video.pause();
+    }
 
     playPauseVideoBtn.addEventListener("change", function () {
         if (playPauseVideoBtn.checked) {
